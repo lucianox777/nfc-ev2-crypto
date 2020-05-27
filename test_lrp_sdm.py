b8853a9ab79726b4d0f2242ca53a601303200699
Michał Leszczyński <ml@icedev.pl>
2020-05-27 18:21:43 +0200

    Add example about LRP SDM with ENCFileData (#5)

--- test_lrp_sdm.py
+++ test_lrp_sdm.py
@@ -56,3 +56,55 @@ def test_lrp_sdm():
     short_tag = bytes(bytearray([full_tag[i] for i in range(16) if i % 2 == 1])).hex()
 
     assert short_tag == cmac.lower()
+
+
+def test_lrp_sdm_with_enc_file():
+    key = binascii.unhexlify("00000000000000000000000000000000")
+
+    # suppose our NDEF URI payload is:
+    # https://any.domain/?m=65628ED36888CF9C84797E43ECACF114C6ED9A5E101EB592
+    # x4ADE304B5AB9474CB40AFFCAB0607A85x87E287E8135BFC06
+
+    msg = "65628ED36888CF9C84797E43ECACF114C6ED9A5E101EB592x4ADE304B5AB9474CB40AFFCAB0607A85x87E287E8135BFC06"
+
+    # break into pieces
+    iv = msg[:16]
+    picc_data, enc_file_data, cmac = msg[16:].split('x')
+
+    # decrypt our message
+    lrp = LRP(key, 0, binascii.unhexlify(iv), pad=False)
+    decrypted_msg = lrp.decrypt(binascii.unhexlify(picc_data))
+    assert decrypted_msg.hex() == "c7042e1d222a63807b00002993571635"
+
+    # create session key
+    # SV = 00h || 01h || 00h || 80h [ || UID] [ || SDMReadCtr] [ || ZeroPadding] || 1Eh || E1h
+    svstream = io.BytesIO()
+    svstream.write(b"\x00\x01\x00\x80")
+    svstream.write(decrypted_msg[1:11])  # UID || SDMReadCtr
+
+    while (svstream.getbuffer().nbytes + 2) % 16 != 0:
+        svstream.write(b"\x00")
+
+    svstream.write(b"\x1E\xE1")
+
+    assert svstream.getbuffer().nbytes % 16 == 0
+
+    # generate master key
+    lrp = LRP(key, 0)
+    master_key = lrp.cmac(svstream.getvalue())
+
+    assert master_key.hex() == "817133dd9fff11b94fc2fb107aa4d971"
+
+    # generate actual MAC_LRP
+    mac_obj = LRP(master_key, 0)
+    # everything in hex since PICCData till the MAC offset
+    msg_no_cmac = "any.domain/?m=65628ED36888CF9C84797E43ECACF114C6ED9A5E101EB592" \
+                  "x4ADE304B5AB9474CB40AFFCAB0607A85x".encode('ascii')
+    full_tag = mac_obj.cmac(msg_no_cmac)
+    short_tag = bytes(bytearray([full_tag[i] for i in range(16) if i % 2 == 1])).hex()
+
+    assert cmac == short_tag.upper()
+
+    # IV = SDMReadCtr || 00 00 00
+    enc_obj = LRP(master_key, 1, r=decrypted_msg[8:11] + b"\x00" * 3, pad=False)
+    assert enc_obj.decrypt(binascii.unhexlify(enc_file_data)).decode('ascii') == "0102030400000000"
